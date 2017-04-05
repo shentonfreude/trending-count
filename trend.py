b1c954c197f0ec6891af43ae970d0a7af9bec7f4
Chris Shenton <chris@v-studios.com>
2017-04-05 14:11:34 -0400

    Derate over time even if we haven't gotten a hit.
    We keep derating the derated value so this may be too 'aggressive'

--- trend.py
+++ trend.py
@@ -22,9 +22,7 @@ now_secs = secs_since_epoch()
 init = {'dt': now_secs, 'score': 0, 'hits': []}
 trends = {'id1': deepcopy(init),
           'id2': deepcopy(init),
-          'id3': deepcopy(init),
-          'id4': deepcopy(init),
-          'id5': deepcopy(init)}
+          }
 print_trends(trends)
 
 # Loop over items, add 0, 1, or 2 "hits", and add to score derating the old
@@ -33,12 +31,13 @@ print_trends(trends)
 while True:
     dt = secs_since_epoch()
     for name, v in trends.items():
+        hits = choice([0, 0, 1])  # 1 in 3 chance of a hit
+        v['hits'].append(hits)
         delta = dt - v['dt']  # number of seconds old
         derate = 1.0 / delta  # DANGER if this is < 1 we grow our score
-        v['dt'] = dt
-        hits = choice([0, 0, 0, 1])
-        v['hits'].append(hits)
         v['score'] = v['score'] * derate + hits
+        if hits:                # update last dt only if we got a hit
+            v['dt'] = dt
         trends[name] = v
     print_trends(trends)
     sleep(1)
