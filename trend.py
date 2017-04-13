28a6dba87e660527c0d17de6b36ec0d796462f0a
Chris Shenton <chris@v-studios.com>
2017-04-13 12:52:59 -0400

    Remove no longer used datetime, sleep, print
    The plots are so much more useful, and we can go with simple intervals
    rather than datestamps.

--- trend.py
+++ trend.py
@@ -1,41 +1,26 @@
 #!/usr/bin/env python
 
 from copy import deepcopy
-from datetime import datetime
 from random import choice
-from time import mktime, sleep
 
-from plotly.graph_objs import Scatter, Figure, Layout
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from plotly.graph_objs import Scatter
+from plotly.offline import plot
 
 INTERVALS = 100
 
 
-def secs_since_epoch():
-    dt = datetime.now()
-    return mktime(dt.timetuple()) + dt.microsecond/1000000.0
-
-
-def print_trends(trends):
-    for name, v in sorted(trends.items()):
-        print('{}: {} {}'.format(name, round(v['score'], 2), v['hits']))
-
-
 # Create structure for one or more items to trend.
-# now_secs = secs_since_epoch()  # just increment from 0
+
 init = {'dt': 0, 'score': 0, 'scores': [], 'hits': []}
 trends = {'id1': deepcopy(init),
           'id2': deepcopy(init),
           }
 dts = [0]
 
-print_trends(trends)
-
 # Loop over items, add 0, 1, or 2 "hits", and add to score derating the old
 # based on last dt.
 
 for dt in range(1, INTERVALS):
-    #dt = secs_since_epoch()
     dts.append(dt)
     for name, v in trends.items():
         hits = choice([0, 0, 1])  # 1 in 3 chance of a hit
@@ -48,10 +33,7 @@ for dt in range(1, INTERVALS):
         if hits:                # update last dt only if we got a hit
             v['dt'] = dt
         trends[name] = v
-    #print_trends(trends)
-    #sleep(0.1)
 
-#import pdb; pdb.set_trace();
 plot(
     [Scatter(x=dts, y=trends['id1']['scores'], name='id1_scores', line={'color':'blue'}),
      Scatter(x=dts, y=trends['id1']['hits'],   name='id1_hits',   mode='markers', marker={'color': 'blue'}),
