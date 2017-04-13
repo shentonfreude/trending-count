02d3a53a5e3562c4e79064cd87f10bfe70edb0f0
Chris Shenton <chris@v-studios.com>
2017-04-13 12:40:11 -0400

    Use Plotly to generate graphs of hits and scores

--- trend.py
+++ trend.py
@@ -5,6 +5,11 @@ from datetime import datetime
 from random import choice
 from time import mktime, sleep
 
+from plotly.graph_objs import Scatter, Figure, Layout
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+INTERVALS = 100
+
 
 def secs_since_epoch():
     dt = datetime.now()
@@ -16,26 +21,40 @@ def print_trends(trends):
         print('{}: {} {}'.format(name, round(v['score'], 2), v['hits']))
 
 
-now_secs = secs_since_epoch()
-init = {'dt': now_secs, 'score': 0, 'hits': []}
+# Create structure for one or more items to trend.
+# now_secs = secs_since_epoch()  # just increment from 0
+init = {'dt': 0, 'score': 0, 'scores': [], 'hits': []}
 trends = {'id1': deepcopy(init),
           'id2': deepcopy(init),
           }
+dts = [0]
+
 print_trends(trends)
 
 # Loop over items, add 0, 1, or 2 "hits", and add to score derating the old
 # based on last dt.
 
-while True:
-    dt = secs_since_epoch()
+for dt in range(1, INTERVALS):
+    #dt = secs_since_epoch()
+    dts.append(dt)
     for name, v in trends.items():
         hits = choice([0, 0, 1])  # 1 in 3 chance of a hit
         v['hits'].append(hits)
         delta = dt - v['dt']  # number of seconds old
         derate = 1.0 / delta  # DANGER if this is < 1 we grow our score
-        v['score'] = v['score'] * derate + hits
+        score = v['score'] * derate + hits
+        v['score'] = score
+        v['scores'].append(score)
         if hits:                # update last dt only if we got a hit
             v['dt'] = dt
         trends[name] = v
-    print_trends(trends)
-    sleep(1)
+    #print_trends(trends)
+    #sleep(0.1)
+
+#import pdb; pdb.set_trace();
+plot(
+    [Scatter(x=dts, y=trends['id1']['scores'], name='id1_scores', line={'color':'blue'}),
+     Scatter(x=dts, y=trends['id1']['hits'],   name='id1_hits',   mode='markers', marker={'color': 'blue'}),
+     Scatter(x=dts, y=trends['id2']['scores'], name='id2_scores', line={'color': 'red'}),
+     Scatter(x=dts, y=trends['id2']['hits'],   name='id2_hits',   mode='markers', marker={'color': 'red'}),
+    ])
